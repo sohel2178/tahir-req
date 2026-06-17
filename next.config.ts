671294df6de86e63e036a65e44061b34bf4da490
Sohel Ahmed <sohel@kali.forbit>
2026-06-17 14:24:10 +0600

    Upgrade to Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,16 @@
 import nextPWA from "next-pwa";
 
-/** STEP 1: configure PWA */
 const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  clientsClaim: true, // 👈 IMPORTANT
-  buildExcludes: [/app-build-manifest\.json$/],
+  clientsClaim: true,
 });
 
-/** STEP 2: normal Next config */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-/** STEP 3: wrap */
 export default withPWA(nextConfig);
